c365a72d7fdf8f6122b8082ced897d8404619d25
Som3a-ai <ismailhelal942@gmail.com>
2026-04-25 17:22:57 +0300

    proxy enhanced

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,34 +1,56 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
-import React from "react";
 
-export async function proxy(request: NextRequest) {
-  const protectedRoutes = ["/cart", "/wishlist", "/checkout", "/allorders"];
 
-  const authRoutes = ["/login", "/register"];
 
-  const mypath = request.nextUrl.pathname;
+export async function proxy(req: NextRequest) {
 
-  const myToken = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-    secureCookie: process.env.NODE_ENV === "production",
-  });
 
-  const token = myToken?.routeToken;
+  // const protectedRoutes = ["/cart", "/wishlist", "/checkout", "/allorders"];
+  // const authRoutes = ["/login", "/register"];
 
-  console.log("myToken",myToken)
-  console.log("token",token)
 
-  if (!token && protectedRoutes.some((path) => mypath.startsWith(path))) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  // const mypath = request.nextUrl.pathname;
+
+  // const myToken = await getToken({
+  //   req: request,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  //   secureCookie: process.env.NODE_ENV === "production",
+  // });
+
+
+
+  // const token = myToken?.routeToken;
+
+ 
+
+  // if (!token && protectedRoutes.some((path) => mypath.startsWith(path))) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+
+  // if (token && authRoutes.some((path) => mypath.startsWith(path))) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
+
+  // return NextResponse.next();
+
+
+
+
+
+
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET
+  })
+
+
+  if(!!token){
+
+    return NextResponse.next();
+  } else{
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}login`)
   }
-
-  if (token && authRoutes.some((path) => mypath.startsWith(path))) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  return NextResponse.next();
 }
 
 export const config = {
